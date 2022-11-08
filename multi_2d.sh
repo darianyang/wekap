f45614b27f8dcd8a86eb1c227abd053ee39f08ec
Darian T. Yang <dty7@pitt.edu>
2022-11-07 23:09:42 -0500

    finished kinetics analysis of 2kod oa v01

--- multi_2d.sh
+++ multi_2d.sh
@@ -2,13 +2,13 @@
 # run multiple kinetics analysis pipelines
 
 #SYSTEMS=(v00 v01 v02 threshv00)
-SYSTEMS=(v00)
+SYSTEMS=(v01)
 # $3 in kinetics pipeline
 #WEST_DIR=2kod_c2_80
 WEST_DIR=2kod_oa
 
 for SYS in ${SYSTEMS[@]} ; do 
-    for ANGLE in {48..58..1} ; do
+    for ANGLE in {48..54..1} ; do
     #for ANGLE in {36..20..1} ; do
         bash kinetics_pipeline_2d_oa.sh $ANGLE $SYS $WEST_DIR
         wait
