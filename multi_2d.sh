fa71a50e501586a13298f4ffbd1959b3e455db73
Darian T. Yang <dty7@pitt.edu>
2022-11-16 12:16:17 -0500

    finished 2kod_oa_65c2

--- multi_2d.sh
+++ multi_2d.sh
@@ -5,10 +5,10 @@
 SYSTEMS=(v00 v01)
 # $3 in kinetics pipeline
 #WEST_DIR=2kod_c2_80
-WEST_DIR=2kod_oa
+WEST_DIR=2kod_oa_65c2
 
 for SYS in ${SYSTEMS[@]} ; do 
-    for ANGLE in {40..52..1} ; do
+    for ANGLE in {45..55..1} ; do
     #for ANGLE in {36..20..1} ; do
         bash kinetics_pipeline_2d_oa.sh $ANGLE $SYS $WEST_DIR
         wait
