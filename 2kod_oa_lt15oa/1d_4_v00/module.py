fa433147fde079678a4c351039fc38a322f9891a
Darian T. Yang <dty7@pitt.edu>
2022-11-22 12:38:53 -0500

    finished into low oa state

---
+++ 2kod_oa_lt15oa/1d_4_v00/module.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import numpy
+
+def pull_data_1d(n_iter, iter_group):
+    '''
+    This function reshapes auxiliary data for each iteration and returns it.
+    '''
+    #auxdata = iter_group['auxdata']['pcoord'][...]
+    auxdata = iter_group['pcoord'][...]
+    #data = auxdata[:,:,numpy.newaxis]
+    data = numpy.atleast_3d(auxdata)
+    return data
+
+def pull_data_2d(n_iter, iter_group):
+    '''
+    This function reshapes 2 auxiliary datasets for each iteration and returns it.
+    '''
+    #auxdata1 = iter_group['auxdata']['pcoord'][...]
+    # special for pcoord
+    auxdata1 = iter_group['pcoord'][...]
+    auxdata2 = iter_group['auxdata'][''][...]
+    data = numpy.dstack((auxdata1, auxdata2))
+    return data
--- multi_1d_oa.sh
+++ multi_1d_oa.sh
@@ -5,10 +5,10 @@
 SYSTEMS=(v00)
 # $3 in kinetics pipeline
 #WEST_DIR=2kod_c2_80
-WEST_DIR=2kod_oa
+WEST_DIR=2kod_oa_lt15oa
 
 for SYS in ${SYSTEMS[@]} ; do 
-    for ANGLE in {48..54..1} ; do
+    for ANGLE in {4..12..1} ; do
     #for ANGLE in {36..20..1} ; do
         bash kinetics_pipeline_1d_oa.sh $ANGLE $SYS $WEST_DIR
         wait
