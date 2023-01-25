220463a70df9b96ce6ee38deff578cf684671b3d
Darian T. Yang <dty7@pitt.edu>
2023-01-25 09:36:50 -0500

    1d oapdt kinetics for std lowpH and 150end

---
+++ multi_2d_oapdt_c2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# run multiple kinetics analysis pipelines
+
+#SYSTEMS=(v00 v01 v02 threshv00)
+#SYSTEMS=(2kod_v00 lo_pH_v00 150end_v00)
+SYSTEMS=(150end_v00)
+# $3 in kinetics pipeline
+WEST_DIR=gt2500oapdt_gt65c2
+
+for SYS in ${SYSTEMS[@]} ; do 
+    for C2 in {65..65} ; do
+        #WEST_DIR=2kod_oa_${C2}c2
+    # o_angle
+    for ANGLE in {2500..2500} ; do
+    #for ANGLE in {36..20..1} ; do
+        bash kinetics_pipeline_2d_oapdt.sh $ANGLE $SYS $WEST_DIR $C2
+        wait
+    done
+    done
+done
