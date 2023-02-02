485196fb26b8b5d251864c3c300360169442b99d
Darian T. Yang <dty7@pitt.edu>
2023-02-02 13:40:32 -0500

    finished a oapdt c2 2d grid

--- multi_2d_oapdt_c2.sh
+++ multi_2d_oapdt_c2.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 # run multiple kinetics analysis pipelines
 
-#SYSTEMS=(v00 v01 v02 threshv00)
-#SYSTEMS=(2kod_v00 lo_pH_v00 150end_v00)
-SYSTEMS=(2kod_v00 lo_pH_v00)
+SYSTEMS=(2kod_v00 lo_pH_v00 150end_v00)
+#SYSTEMS=(2kod_v00 lo_pH_v00)
 #SYSTEMS=(150end_v00)
+
 # $3 in kinetics pipeline
-WEST_DIR=gt2500oapdt_gt65c2
+WEST_DIR=oapdt_c2_2dgrid
 
 for SYS in ${SYSTEMS[@]} ; do 
-    for C2 in {65..65} ; do
+    for C2 in {66..72} ; do
         #WEST_DIR=2kod_oa_${C2}c2
     # o_angle
-    for ANGLE in {2500..2500} ; do
+    for ANGLE in {2100..2600..100} ; do
     #for ANGLE in {36..20..1} ; do
         bash kinetics_pipeline_2d_oapdt.sh $ANGLE $SYS $WEST_DIR $C2
         wait
