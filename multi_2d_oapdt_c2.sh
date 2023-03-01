40de9a741745134e9564a2a89cb4bec270afba2d
Darian T. Yang <dty7@pitt.edu>
2023-03-01 13:50:18 -0500

    ran 70 2500 for 4f and 7f 1d oapdt

--- multi_2d_oapdt_c2.sh
+++ multi_2d_oapdt_c2.sh
@@ -2,15 +2,16 @@
 # run multiple kinetics analysis pipelines
 
 #SYSTEMS=(2kod_v00 lo_pH_v00 150end_v00)
-SYSTEMS=(2kod_v00)
+#SYSTEMS=(2kod_v00)
 #SYSTEMS=(150end_v00)
+SYSTEMS=(4F_v00 7F_v00)
 
 # $3 in kinetics pipeline
 WEST_DIR=oapdt_c2_2dgrid
 
 for SYS in ${SYSTEMS[@]} ; do 
     #for C2 in {66..72} ; do
-    for C2 in {71..72} ; do
+    for C2 in {70..70} ; do
         #WEST_DIR=2kod_oa_${C2}c2
     # o_angle
     #for ANGLE in {2100..2600..100} ; do
