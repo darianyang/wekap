0f9f8f8f28d9b15ad3028e494daab54617bd9582
Darian T. Yang <dty7@pitt.edu>
2023-03-28 16:16:56 -0400

    ran 2d oamax c2 of wt v00 oamax run

---
+++ multi_2d_oamax_c2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# run multiple kinetics analysis pipelines
+
+#SYSTEMS=(2kod_v00 lo_pH_v00 150end_v00)
+#SYSTEMS=(2kod_v00)
+#SYSTEMS=(150end_v00)
+SYSTEMS=(WT_v00)
+
+# $3 in kinetics pipeline
+WEST_DIR=oapdt_c2_2dgrid
+
+for SYS in ${SYSTEMS[@]} ; do 
+    #for C2 in {66..72} ; do
+    for C2 in {70..70} ; do
+        #WEST_DIR=2kod_oa_${C2}c2
+    # o_angle
+    for ANGLE in {50..50} ; do
+        bash kinetics_pipeline_2d_oamax.sh $ANGLE $SYS $WEST_DIR $C2
+        wait
+    done
+    done
+done
