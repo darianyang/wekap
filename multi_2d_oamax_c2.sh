9ed70523f9f74c9e274e56484ab62cd23a154418
Darian T. Yang <dty7@pitt.edu>
2023-04-18 11:15:21 -0400

    oamax grid 2 finished

--- multi_2d_oamax_c2.sh
+++ multi_2d_oamax_c2.sh
@@ -7,14 +7,14 @@
 SYSTEMS=(WT_v00)
 
 # $3 in kinetics pipeline
-WEST_DIR=oamax_c2_2dgrid
+WEST_DIR=oamax_c2_2dgrid_2
 
 for SYS in ${SYSTEMS[@]} ; do 
     #for C2 in {66..72} ; do
-    for C2 in {70..78} ; do
+    for C2 in {72..80} ; do
         #WEST_DIR=2kod_oa_${C2}c2
     # o_angle
-    for ANGLE in {50..55} ; do
+    for ANGLE in {48..54} ; do
         bash kinetics_pipeline_2d_oamax.sh $ANGLE $SYS $WEST_DIR $C2
         wait
     done
