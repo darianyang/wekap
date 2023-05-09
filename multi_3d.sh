1bf6f107781613be953787be7edf973200847597
Darian T. Yang <dty7@pitt.edu>
2023-05-09 13:10:39 -0400

    added finished grid of oa1 oa2 and c2

--- multi_3d.sh
+++ multi_3d.sh
@@ -2,8 +2,8 @@
 # run multiple kinetics analysis pipelines
 
 #SYSTEMS=(WT_v00)
-#SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04 4F_v00 7F_v00)
-SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04)
+SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04 4F_v00 7F_v00)
+#SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04)
 #SYSTEMS=(4F_v00 7F_v00)
 
 # $3 in kinetics pipeline
@@ -12,7 +12,7 @@ WEST_DIR=oa1_oa2_c2
 for SYS in ${SYSTEMS[@]} ; do 
     for C2 in {72..72} ; do
     # o_angle : 52, 55, 60
-    for ANGLE in {55..55} ; do
+    for ANGLE in {56..59} ; do
         bash kinetics_pipeline_3d.sh $ANGLE $SYS $WEST_DIR $C2
         wait
     done
