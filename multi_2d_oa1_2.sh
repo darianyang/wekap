72aa906aaa975478217345aa0ddd8dd86bdf939d
Darian T. Yang <dty7@pitt.edu>
2024-03-14 09:29:57 -0400

    added 4d v01 13

--- multi_2d_oa1_2.sh
+++ multi_2d_oa1_2.sh
@@ -6,14 +6,14 @@
 #SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04)
 #SYSTEMS=(4F_v00 7F_v00)
 #SYSTEMS=(4F_v01 4F_v02 4F_v03 4F_v04 7F_v01 7F_v02 7F_v03 7F_v04)
-SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04 4F_v00 4F_v01 4F_v02 4F_v03 4F_v04 7F_v00 7F_v01 7F_v02 7F_v03 7F_v04)
+#SYSTEMS=(WT_v00 WT_v01 WT_v02 WT_v03 WT_v04 4F_v00 4F_v01 4F_v02 4F_v03 4F_v04 7F_v00 7F_v01 7F_v02 7F_v03 7F_v04)
 #SYSTEMS=(3d_v00 3d_v01)
-#SYSTEMS=(7F_v03)
+SYSTEMS=(4F_v01)
 
 for SYS in ${SYSTEMS[@]} ; do 
     # o_angle 
-    for ANGLE in {11..15..2} ; do
-    #for ANGLE in {14..14} ; do
+    #for ANGLE in {11..15..2} ; do
+    for ANGLE in {13..13} ; do
         bash kinetics_pipeline_2d_oa1_2.sh $ANGLE $SYS 
         wait
     done
